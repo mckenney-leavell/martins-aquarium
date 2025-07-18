4621fca6b6915b6a1e1b252f161951a89dda23fa
McKenney Leavell <maleavell@gmail.com>
2025-07-18 16:01:31 -0500

    Updated main.css file

--- scripts/aquariumData.js
+++ scripts/aquariumData.js
@@ -6,7 +6,7 @@ export const database = {
             length: 3,
             location: "Great Barrier Reef",
             diet: "Omnivore",
-            image: "https://example.com/images/nemo.jpg"
+            image: "https://i0.wp.com/fishkeepingnews.com/wp-content/uploads/2020/03/Falsche_Clownfisch_Amphiprion_ocellaris.jpg?resize=800%2C445&ssl=1"
         },
         {
             name: "Dory",
@@ -14,7 +14,7 @@ export const database = {
             length: 5,
             location: "Great Barrier Reef",
             diet: "Omnivore",
-            image: "https://example.com/images/dory.jpg"
+            image: "https://www.cincinnaticoral.com/cdn/shop/products/bluehippo_1024x1024@2x.jpg?v=1585108167"
         },
                 {
             name: "Mike",
@@ -25,12 +25,12 @@ export const database = {
             image: "https://www.treehugger.com/thmb/fGCoMcqJcbRhPD8prjKueIjQPR4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-1159240457-79e9779a466c4b6e82d188384c34142b.jpg"
         },
         {
-            name: "Goldie",
-            species: "Goldfish",
-            length: 2,
+            name: "Amy",
+            species: "Rainbowfish",
+            length: 3,
             location: "Great Barrier Reef",
             diet: "Omnivore",
-            image: "https://www.squiresgardencentres.co.uk/shop/gallery/Red-Goldfish_shutterstock_417478-medium.jpg"
+            image: "https://static.wixstatic.com/media/a24a53_a96c116b2d9b45fe817afafcbea97d7e~mv2.jpg/v1/fill/w_740,h_416,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/a24a53_a96c116b2d9b45fe817afafcbea97d7e~mv2.jpg"
         },
                 {
             name: "Sarah",
@@ -47,14 +47,6 @@ export const database = {
             location: "Great Barrier Reef",
             diet: "Omnivore",
             image: "https://cdn.shopify.com/s/files/1/0311/3149/files/corydoras_sterbai.jpg?v=1605035087"
-        },
-        {
-            name: "Amy",
-            species: "Rainbowfish",
-            length: 3,
-            location: "Great Barrier Reef",
-            diet: "Omnivore",
-            image: "https://static.wixstatic.com/media/a24a53_a96c116b2d9b45fe817afafcbea97d7e~mv2.jpg/v1/fill/w_740,h_416,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/a24a53_a96c116b2d9b45fe817afafcbea97d7e~mv2.jpg"
         },
     ],
 
